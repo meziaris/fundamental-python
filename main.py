3552fd8e10ceaacea3af24766d43c7cf9128404e
meziaris <meziaris@gmail.com>
2020-08-01 15:21:24 +0700

    fix if else main.py

--- main.py
+++ main.py
@@ -30,5 +30,7 @@ try:
         print(f'Title: {soup.title.string}')
         print(f'Isi {url} adalah (dalam tag p):')
         print(soup.p.get_text())
+    else:
+        print(f'Woops, something wrong! error {response.status_code}')
 except Exception as e:
     print(f'There is an error {e}')
